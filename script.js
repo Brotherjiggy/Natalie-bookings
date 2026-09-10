61141e64b79dcf42b3eadbdb8fb9f6d0e02997e0
Brotherjiggy <emmyjones813@gmail.com>
2026-09-10 04:55:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -83,6 +83,7 @@ function initializeAutoHideHeader() {
     }, { passive: true });
 }
 
+
 /* =========================================================
    SUPABASE INITIALIZATION
 ========================================================= */
